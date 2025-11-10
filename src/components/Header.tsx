8bf276402da36345e2ae37a7b775203d1492dd71
NeilDev1976 <neil.surridge@hotmail.co.uk>
2025-11-10 21:49:22 +0000

    feat (layout) : added basic layout and light mode styling. Also GitHub link and logo. json files for displayed data (#8)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,35 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import siteConfig from "../data/siteConfig.json";
+import GitHubLogo from  "../assets/GitHub_Invertocat_Dark.svg";
 
 const Header = () => {
+    
+    // Render menu items, can be used in both mobile and desktop navs, load from siteConfig
+    const renderMenu = () => (
+      siteConfig.navigation.map((item) => (
+          <NavLink to={item.path}
+            className={({isActive}) => `${isActive ? "border-b-4 border-grey-900" : "border-b-0"}`}>
+            {item.name}
+          </NavLink>
+          ))
+      );
+
     return (
-        <header>
-            <nav>
-                
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                
-            </nav>
+        <header className="fixed top-0 left-0 w-full flex justify-center z-50 pointer-events-none">
+            <div className="pointer-events-auto bg-gray-100 rounded-2xl px-8 py-2 mt-4 shadow-md">
+                <nav className="flex gap-6 text-gray-900">
+                    {renderMenu()}
+
+                    <a
+                        href={siteConfig.site.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:opacity-80"
+                    >
+                        <img src={GitHubLogo} alt="GitHub" className="h-7 w-7" />
+                    </a>
+                </nav>
+            </div>
         </header>
     );
 }
